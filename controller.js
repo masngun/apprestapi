d3fdb27620a48d38744a4a981876d6d3e5b634e5
unknown <herwanto.bangun@gmail.com>
2022-04-25 19:29:42 +0700

    update file controler dan server

--- controller.js
+++ controller.js
@@ -4,5 +4,5 @@ var response = require ('./res');
 var connection = require ('./koneksi');
 
 exports.index = function (req,res) {
-    response.ok("Applikasi REST API ku berjalan!")
+    response.ok("Applikasi REST API ku berjalan!",res)
 };
