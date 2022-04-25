56d4f6931e10be749237968da1f4339026061b87
unknown <herwanto.bangun@gmail.com>
2022-04-25 22:42:11 +0700

    menambahkan controller dan route tampil mahasiswa all

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require ('./koneksi');
 exports.index = function (req,res) {
     response.ok("Applikasi REST API ku berjalan!",res)
 };
+
+// menampilkan semua data mahasiswa
+exports.tampilsemuamahasiswa = function(req,res) {
+    connection.query('SELECT * FROM mahasiswa', function(error, rows, field){
+        if(error) {
+            console.log(error);
+        } else {
+            response.ok(rows, res)
+        }
+    });
+}
